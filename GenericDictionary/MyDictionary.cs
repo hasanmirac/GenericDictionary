e7d96d8caa89de522cf61cc0fd1600c95df609f5
hasanmirac <61428133+hasanmirac@users.noreply.github.com>
2021-01-26 14:40:54 +0300

    Add project files.

---
+++ GenericDictionary/MyDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] _keyArray;
+        TValue[] _valueArray;
+
+        TKey[] _tempKeyArray;
+        TValue[] _tempValueArray;
+
+        public MyDictionary()
+        {
+            _keyArray = new TKey[0];
+            _valueArray = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            _tempKeyArray = _keyArray;
+            _tempValueArray = _valueArray;
+
+            _keyArray = new TKey[_keyArray.Length + 1];
+            _valueArray = new TValue[_valueArray.Length + 1];
+
+            for (int i = 0; i < _tempKeyArray.Length; i++)
+            {
+                _keyArray[i] = _tempKeyArray[i];
+            }
+
+            for (int i = 0; i < _tempValueArray.Length; i++)
+            {
+                _valueArray[i] = _tempValueArray[i];
+            }
+
+            _keyArray[_keyArray.Length - 1] = key;
+            _valueArray[_valueArray.Length - 1] = value;
+
+        }
+
+        public TKey[] Items1
+        {
+            get { return _keyArray; }
+        }
+
+        public TValue[] Items2    
+        {
+            get { return _valueArray; }
+
+        }
+    }
+}
---
+++ GenericDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> plaka = new MyDictionary<int, string>();
+            plaka.Add(61, "Trabzon");
+            plaka.Add(44, "Malatya");
+            plaka.Add(35, "İzmir");
+            plaka.Add(34, "İstanbul");
+
+            Console.WriteLine("Plaka : " + plaka.Items1[0] + "  " + "Şehir : " + plaka.Items2[0] );
+            Console.WriteLine("Plaka : " + plaka.Items1[1] + "  " + "Şehir : " + plaka.Items2[1]);
+            Console.WriteLine("Plaka : " + plaka.Items1[2] + "  " + "Şehir : " + plaka.Items2[2]);
+            Console.WriteLine("Plaka : " + plaka.Items1[3] + "  " + "Şehir : " + plaka.Items2[3]);
+
+        }
+    }
+}
